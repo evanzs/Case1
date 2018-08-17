e7d8e6f7b906be0413559665bc7c5f4b84278533
evanzs <evandrofidk@hotmail.com>
2018-08-16 22:00:21 -0300

    Instalando Bootstrap e forms

--- Case1/src/app/app.module.ts
+++ Case1/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AppComponent } from './app.component';
-
+import {CadastroFormComponent} from './cadastro-form/cadastro-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+  CadastroFormComponent
+    
   ],
   imports: [
+   
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ Case1/src/app/cadastro-form/cadastro-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cadastro-form',
+  templateUrl: './cadastro-form.component.html',
+  styleUrls: ['./cadastro-form.component.css']
+})
+export class CadastroFormComponent  {
+
+ 
+
+}
